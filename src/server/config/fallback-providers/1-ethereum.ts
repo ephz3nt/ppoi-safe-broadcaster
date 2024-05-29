48f4402cd1f3deb2e2ca67fec09b959a9ad208e6
ephz3nt <blijf1993@gmail.com>
2024-05-29 10:20:45 +0800

    rpc

--- src/server/config/fallback-providers/1-ethereum.ts
+++ src/server/config/fallback-providers/1-ethereum.ts
@@ -4,7 +4,7 @@ const config: FallbackProviderJsonConfig = {
   chainId: 1,
   providers: [
     {
-      provider: 'https://ethereum-rpc.publicnode.com',
+      provider: 'https://eth.merkle.io',
       priority: 2,
       weight: 2,
       maxLogsPerBatch: 10,
@@ -14,26 +14,36 @@ const config: FallbackProviderJsonConfig = {
       provider: 'https://1rpc.io/eth',
       priority: 3,
       weight: 2,
+      maxLogsPerBatch: 10,
+      stallTimeout: 2500,
     },
     {
       provider: 'https://rpc.flashbots.net',
       priority: 3,
       weight: 2,
+      maxLogsPerBatch: 10,
+      stallTimeout: 2500,
     },
     {
-      provider: 'https://eth.merkle.io',
+      provider: 'https://ethereum-rpc.publicnode.com',
       priority: 3,
       weight: 2,
+      maxLogsPerBatch: 10,
+      stallTimeout: 2500,
     },
     {
       provider: 'https://eth.llamarpc.com',
       priority: 3,
       weight: 2,
+      maxLogsPerBatch: 10,
+      stallTimeout: 2500,
     },
     {
       provider: 'https://cloudflare-eth.com/',
       priority: 3,
       weight: 1,
+      maxLogsPerBatch: 10,
+      stallTimeout: 2500,
     },
   ],
 };
--- src/server/config/fallback-providers/137-polygon-pos.ts
+++ src/server/config/fallback-providers/137-polygon-pos.ts
@@ -4,14 +4,14 @@ const config: FallbackProviderJsonConfig = {
   chainId: 137,
   providers: [
     {
-      provider: 'https://rpc.ankr.com/polygon',
+      provider: 'https://polygon-bor-rpc.publicnode.com',
       priority: 2,
       weight: 2,
       maxLogsPerBatch: 1, // Supports up to 10, but at 1 ethers handles getLogs differently, and this seems to be more stable.
       stallTimeout: 2500,
     },
     {
-      provider: 'https://polygon-rpc.com',
+      provider: 'https://polygon.llamarpc.com',
       priority: 3,
       weight: 1,
     },
